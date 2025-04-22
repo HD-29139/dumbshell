b197f5563062b33e0836c6b05a1691a75624dc91
HD-29139 <santanajefferson594@gmail.com>
2025-04-22 03:27:40 -0300

    working with strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, stdout, Write};
+use std::process::Command;
 fn main(){
-    
+    loop {
+        print!("dumbshell>");
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        if command == "quit" || command == "q" {
+            break;
+        }else {
+            Command::new(command).args(args).status().unwrap();
+        }
+    }
 }
