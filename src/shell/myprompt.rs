d1299c68864ab77906a18d3e93078a2106718afd
HD-29139 <santanajefferson594@gmail.com>
2025-04-25 02:05:34 -0300

    better name and some colors

---
+++ src/shell/myprompt.rs
@@ -0,0 +1,21 @@
+use colored::*;
+use std::env;
+
+pub fn gt_prompt() -> std::io::Result<String> {
+    let dir = env::current_dir()?;
+
+    let path = dir
+        .file_name()
+        .and_then(|name| name.to_str())
+        .unwrap_or("/");
+
+    let path_colored = format!("{}", path.on_magenta().black());
+    let prompt = "> ".yellow();
+    let hour = chrono::Local::now()
+        .format("%H:%M")
+        .to_string()
+        .blue()
+        .bold();
+
+    Ok(format!("[{}] {} {}", hour, path_colored, prompt))
+}
