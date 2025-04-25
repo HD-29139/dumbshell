8e32c8ef88180673750e0f05bb7713ca2e4f8659
HD-29139 <santanajefferson594@gmail.com>
2025-04-25 03:00:38 -0300

    bye...

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,12 +1,9 @@
 pub mod myprompt;
-// pub mod commands;
 
-// use commands::{color_cmd, handle_};
-use myprompt::gt_prompt;
-use std::env;
-use std::io::{stdin, stdout, Error, Write};
-use std::process::Command;
 use colored::*;
+use myprompt::gt_prompt;
+use std::io::{Write, stdout};
+use std::process::Command;
 
 pub fn run_shell() -> std::io::Result<()> {
     loop {
@@ -18,19 +15,17 @@ pub fn run_shell() -> std::io::Result<()> {
         std::io::stdin().read_line(&mut input).unwrap();
 
         let mut parts = input.trim().split_whitespace();
-        let command = match parts.next() {
-            Some(command) => command,
-            None => continue,
-        };
+        let command = parts.next().unwrap();
         let args = parts;
 
         let mut child = match Command::new(command).args(args).spawn() {
             Ok(child) => child,
             Err(e) => {
-                println!("{}", e.to_string().red());
+                println!("aqui: {}", e.to_string().red());
                 continue;
             }
         };
-        child.wait().unwrap();
+        child.wait()?;
     }
+
 }
--- src/shell/myprompt.rs
+++ src/shell/myprompt.rs
@@ -7,7 +7,7 @@ pub fn gt_prompt() -> std::io::Result<String> {
     let path = dir
         .file_name()
         .and_then(|name| name.to_str())
-        .unwrap_or("/");
+        .unwrap_or("/home/ArchJefferson");
 
     let path_colored = format!("{}", path.on_magenta().black());
     let prompt = "> ".yellow();
