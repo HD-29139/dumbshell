8f793051967901e4a32f2090d08f945d05c9a69a
HD-29139 <santanajefferson594@gmail.com>
2025-04-25 01:42:05 -0300

    moved to mod

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,5 @@ use colored::Colorize;
 
 
 fn main() -> std::io::Result<()>{
-    loop {
-       
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-
-        let mut parts = input.trim().split_whitespace();
-        let command = match parts.next() {
-            Some(command) => command,
-            None => continue
-        };
-        let args = parts;
-
-        let mut child = match Command::new(command).args(args).spawn() {
-            Ok(child) => child,
-            Err(e) => {
-                println!("{}", e);
-                continue;
-            }
-        };
-        child.wait().unwrap();
-    }
+    
 }
