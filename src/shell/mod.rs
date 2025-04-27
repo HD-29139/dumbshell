83b802c1798d72dd3a88ebf9f253521b95e865f4
HD-29139 <santanajefferson594@gmail.com>
2025-04-27 00:43:51 -0300

    error handling, damn

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -27,12 +27,17 @@ pub fn run_shell() -> std::io::Result<()> {
                 if let Err(e) = env::set_current_dir(&root) {
                     eprintln!("{}", e);
                 }
-            }
+            },
+
+            "exit" => return Ok(()),
 
             command => {
-                let mut child = Command::new(command).args(args).spawn()?;
+                let child = Command::new(command).args(args).spawn();
 
-                child.wait()?;
+                match child {
+                    Ok(mut child) => {child.wait()?;},
+                    Err(e) => eprintln!("{}", e), 
+                }
             }
         }
     }
